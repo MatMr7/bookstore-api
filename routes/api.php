d2973d8c67b192773f9fb8349696552fe4ff0ffb
Matheus Morais <ti.matheus.morais@gmail.com>
2024-05-18 12:37:26 -0300

    :sparkles: User register route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('register', [UserController::class, 'create']);
 
 Route::get('/', function () {
     return response()->json(['message' => 'ok']);
