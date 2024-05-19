b254f99672051f9226dd3ac1eb2594d2b07a198f
Matheus Morais <ti.matheus.morais@gmail.com>
2024-05-18 22:22:25 -0300

    :white_check_mark: add book relation in store model

--- app/Domain/Store/Models/Store.php
+++ app/Domain/Store/Models/Store.php
@@ -2,7 +2,9 @@
 
 namespace App\Domain\Store\Models;
 
+use App\Domain\Book\Models\Book;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Store extends Model
 {
@@ -15,4 +17,9 @@ class Store extends Model
     protected $casts = [
         'is_active' => 'boolean'
     ];
+
+    public function books(): BelongsToMany
+    {
+        return $this->belongsToMany(Book::class, 'book_store');
+    }
 }
